2ad3c7d2bbf39211ae2436f7669c34027015b71c
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-06-09 00:29:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(superbowls) {
+    const winningGame = superbowls.find((game) => game.result === "W");
+    return winningGame ? winningGame.year : undefined;
+  }
+  
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+
+  const winnerYear = superbowlWin(record);
+  console.log(winnerYear);
